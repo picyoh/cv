b3814675881e015ed0f397102c34679ee44275ba
picyoh <picotyoh@gmail.com>
2022-04-01 18:34:07 +0200

    style: :lipstick: fontAwesome
    add icons

--- src/components/timeLine/TimeLine.jsx
+++ src/components/timeLine/TimeLine.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBook, faGraduationCap, faFilec, faBookReader } from '@fortawesome/free-solid-svg-icons'
+import { faHtml5, faCss3Alt, faJs, faReact, faNodeJs, faCss3Alt } from '@fortawesome/free-brands-svg-icons'
 
 import "./timeLine.css"
 
 function TimeLine(props) {
 
     const { datas } = props;
+
+    const icons = [
+        { certification: <FontAwesomeIcon icon="fa-solid fa-file-certificate" /> },
+        { cap: <FontAwesomeIcon icon={ faGraduationCap } /> },
+        { books: <FontAwesomeIcon icon={ faBookReader } /> },
+        { html: <FontAwesomeIcon icon={ faHtml5 } /> },
+        { css: <FontAwesomeIcon icon={ faCss3Alt } /> },
+        { js: <FontAwesomeIcon icon={ faJs } /> },
+        { nodejs: <FontAwesomeIcon icon={ faNodeJs } /> },
+        { react: <FontAwesomeIcon icon={ faReact } /> },
+
+    ]
+
     const list = datas.map((element, index) => {
         return <li key={index}>
             <span className="year-item">
@@ -12,6 +28,10 @@ function TimeLine(props) {
             </span>
             <span className="infos">
                 {element.infos} .
+                {icons.books}
+                {icons.diploma}
+                {icons.cap}
+                {icons.html}
             </span>
         </li>
     });
