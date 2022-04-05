22bbd834b3b72b09f3187d0498b73af50c8670db
picyoh <picotyoh@gmail.com>
2022-04-06 01:37:16 +0200

    feat: :sparkles: SkillPie
    add SkillPie component

---
+++ src/components/skillPie/SkillPie.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { PieChart, Pie, Cell } from 'recharts'
+
+function SkillPie(props) {
+
+    const { skill } = props;
+
+    // Set transparent Cell 
+    const data = [
+        { name: "full", value: skill.prc, },
+        { name: "empty", value: 100 - skill.prc }
+    ]
+    // Set 
+    const colors = []
+    // TODO: Color Array for full and empty
+    // TODO: customize labels with Icon
+    // TODO: autosizing PieChart and Pie
+    return (
+        <PieChart width={144} height={144} >
+            <Pie
+                dataKey="value"
+                data={data}
+                startAngle={180}
+                endAngle={-180}
+                outerRadius={64}
+                innerRadius={48}
+                cx={72}
+                cy={72}
+                isAnimationActive={false}
+                fill={"#E63946"}
+            >
+                {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={getColor(index)} />
+                ))}
+            </Pie>
+        </PieChart >
+    )
+}
+
+export default SkillPie
