6bbdf3176bd0f8c2c483073279478576dba21edd
picyoh <picotyoh@gmail.com>
2022-05-05 18:20:52 +0200

    refactor: :lipstick: aboutme
    convert to sass

--- src/sections/aboutme/Aboutme.jsx
+++ src/sections/aboutme/Aboutme.jsx
@@ -6,15 +6,15 @@ import "./aboutme.css"
 function AboutMe() {
     return (
         <section className='aboutMe'>
-            <div className="photo">
+            <div className="aboutme__picture">
                 <img src={logo} alt="Yohann Picot" />
             </div>
-            <div className="intro">
-                <div className="id">
+            <div className="aboutme__intro">
+                <div className="aboutme__intro__id">
                     <h1>Yohann Picot</h1>
                     <h2>Developpeur Front-End</h2>
                 </div>
-                <div className="text">
+                <div className="aboutme__intro__text">
                     <p>
                         Curieux et volontaire, j'aime expérimenter et apprendre de nouvelles choses.
                         Mon parcours atypique me permet de m'adapter et de m'integrer assez vite a de nouveaux environements.
