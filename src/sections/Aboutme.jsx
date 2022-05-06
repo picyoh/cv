ebdc2a8e31310345bb5d69aba3513f184b98438d
picyoh <picotyoh@gmail.com>
2022-05-06 15:32:56 +0200

    refactor: :lipstick: aboutme
    responsive Mobile S sizing

--- src/sections/Aboutme.jsx
+++ src/sections/Aboutme.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
-import logo  from "../assets/illuoimnow.jpg"
+import logo from "../assets/illuoimnow.jpg"
 
 function AboutMe() {
     return (
         <section className='aboutme'>
-            <div className="aboutme__picture">
-                <img src={logo} alt="Yohann Picot" />
-            </div>
-            <div className="aboutme__intro">
-                <div className="aboutme__intro__id">
+            <div className="aboutme__top">
+                <div className="aboutme__top__picture">
+                    <img src={logo} alt="Yohann Picot" />
+                </div>
+                <div className="aboutme__top__id">
                     <h1>Yohann Picot</h1>
                     <h2>Developpeur Front-End</h2>
                 </div>
-                <div className="aboutme__intro__text">
-                    <p>
-                        Curieux et volontaire, j'aime expérimenter et apprendre de nouvelles choses.
-                        Mon parcours atypique me permet de m'adapter et de m'integrer assez vite a de nouveaux environements.
-                    </p>
-                </div>
+            </div>
+            <div className="aboutme__bottom">
+                <p>
+                    Curieux et volontaire, j'aime expérimenter et apprendre de nouvelles choses.
+                    Mon parcours atypique me permet de m'adapter et de m'integrer assez vite a de nouveaux environements.
+                </p>
             </div>
         </section>
     )
